4918d09f1c305418b062f1f0a579a1fe9e8821ca
fbronchaly <fbronchaly@gmail.com>
2020-05-09 11:32:07 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CalculosComponent } from './calculos/calculos.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, CalculosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/calculos/calculos.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculos',
+  templateUrl: './calculos.component.html',
+  styleUrls: ['./calculos.component.css']
+})
+export class CalculosComponent implements OnInit {
+
+
+
+
+estadio;
+c;
+
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+let compara = ()=>{
+
+if ( 90<this.estadio &&  this.proteinuria<30){
+  this.resultado = "ALTA";
+} else if (60< this.estadio || this.estadio >89  &&  this.proteinuria<30 ){
+  this.resultado = "ANUAL";
+}
+else if (30< this.estadio || this.estadio >59  &&  this.proteinuria<30 ){
+  this.resultado = "SEMESTRAL";
+}
+else if (15< this.estadio || this.estadio >29  &&  this.proteinuria<30 ){
+  this.resultado = "TRIMESTRAL";
+}
+else if (15>this.estadio &&  this.proteinuria<30 ){
+  this.resultado = "MESUAL";
+}
+else if (60< this.estadio || this.estadio >89  &&  this.proteinuria>=30   ){
+  this.resultado = "ANUAL";
+}
+else {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
